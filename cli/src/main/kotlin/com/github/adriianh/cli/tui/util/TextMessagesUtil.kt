c7064f06f3832052b2532db63d77a99da9f3274e
adriiianhh <adriianh@proton.me>
2026-03-04 11:10:08 -0600

    refactor(tui): extract shared greeting and duration utils

---
+++ cli/src/main/kotlin/com/github/adriianh/cli/tui/util/TextMessagesUtil.kt
@@ -0,0 +1,16 @@
+package com.github.adriianh.cli.tui.util
+
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
+import kotlin.time.Clock
+
+object TextMessagesUtil {
+    fun buildGreeting(): String {
+        val hour = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault()).hour
+        return when {
+            hour < 12 -> "  Good morning"
+            hour < 18 -> "  Good afternoon"
+            else      -> "  Good evening"
+        }
+    }
+}
