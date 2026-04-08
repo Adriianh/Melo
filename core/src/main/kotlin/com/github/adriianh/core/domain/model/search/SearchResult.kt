72a1841fbf2bd3203421df05dd707a6aeb6dad48
adriiianhh <adriianh@proton.me>
2026-04-07 18:34:30 -0600

    refactor: improve formatting and structure of SearchResult.kt

--- core/src/main/kotlin/com/github/adriianh/core/domain/model/search/SearchResult.kt
+++ core/src/main/kotlin/com/github/adriianh/core/domain/model/search/SearchResult.kt
@@ -1,5 +1,7 @@
 package com.github.adriianh.core.domain.model.search
+
 import com.github.adriianh.core.domain.model.Track
+
 sealed interface SearchResult {
     data class Song(val track: Track) : SearchResult
     data class Album(val id: String, val title: String, val author: String, val year: String?, val artworkUrl: String?) : SearchResult
