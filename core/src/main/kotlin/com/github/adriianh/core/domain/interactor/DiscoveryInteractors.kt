9d602e7c3e8d4980d65095479327c15545f6bb86
adriiianhh <adriianh@proton.me>
2026-04-19 23:20:07 -0600

    feat(core): implement discovery use cases and interactors

---
+++ core/src/main/kotlin/com/github/adriianh/core/domain/interactor/DiscoveryInteractors.kt
@@ -0,0 +1,13 @@
+package com.github.adriianh.core.domain.interactor
+
+import com.github.adriianh.core.domain.usecase.search.GetExploreUseCase
+import com.github.adriianh.core.domain.usecase.search.GetHomeUseCase
+import com.github.adriianh.core.domain.usecase.search.GetRadioUseCase
+import com.github.adriianh.core.domain.usecase.search.GetTrendingUseCase
+
+data class DiscoveryInteractors(
+    val getHome: GetHomeUseCase,
+    val getExplore: GetExploreUseCase,
+    val getTrending: GetTrendingUseCase,
+    val getRadio: GetRadioUseCase
+)
