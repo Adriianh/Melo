40963584a22e6d52d44c9b7958ac76c97338a09a
adriiianhh <adriianh@proton.me>
2026-04-30 19:39:41 -0600

    refactor: move PlaybackState definition from MeloPlayer.kt

--- core/src/commonMain/kotlin/com/github/adriianh/core/domain/player/MeloPlayer.kt
+++ core/src/commonMain/kotlin/com/github/adriianh/core/domain/player/MeloPlayer.kt
@@ -3,15 +3,6 @@ package com.github.adriianh.core.domain.player
 import com.github.adriianh.core.domain.model.Track
 import kotlinx.coroutines.flow.StateFlow
 
-data class PlaybackState(
-    val currentTrack: Track? = null,
-    val isPlaying: Boolean = false,
-    val isBuffering: Boolean = false,
-    val progressMs: Long = 0L,
-    val durationMs: Long = 0L,
-    val error: String? = null
-)
-
 interface MeloPlayer {
     val state: StateFlow<PlaybackState>
 
