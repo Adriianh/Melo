0b775409e061e1aed572d221c97d9585cc6c8681
adriiianhh <adriianh@proton.me>
2026-09-06 21:49:58 -0600

    feat(util): add `WindowsTitleBar` for immersive dark mode and integrate into app window
    - Implemented `WindowsTitleBar` utility to apply immersive dark mode and custom caption colors for Windows 10/11 title bars.
    - Integrated dark mode styling into the app's main window using `LaunchedEffect`.

--- composeApp/src/jvmMain/kotlin/com/github/adriianh/melo/Main.kt
+++ composeApp/src/jvmMain/kotlin/com/github/adriianh/melo/Main.kt
@@ -1,22 +1,35 @@
 package com.github.adriianh.melo
 
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import com.github.adriianh.core.domain.player.MediaSessionManager
+import com.github.adriianh.core.domain.player.MeloPlayer
 import com.github.adriianh.melo.di.initKoin
+import com.github.adriianh.melo.util.WindowsTitleBar
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 fun main() {
     val koinApp = initKoin()
-    val mediaSessionManager: MediaSessionManager? = koinApp.koin.getOrNull()
+
+    CoroutineScope(Dispatchers.IO).launch {
+        koinApp.koin.getOrNull<MeloPlayer>()
+        koinApp.koin.getOrNull<MediaSessionManager>()
+    }
 
     application {
         Window(
             onCloseRequest = {
-                mediaSessionManager?.release()
+                koinApp.koin.getOrNull<MediaSessionManager>()?.release()
                 exitApplication()
             },
             title = "Melo"
         ) {
+            LaunchedEffect(window) {
+                WindowsTitleBar.applyDarkTheme(window)
+            }
             App()
         }
     }
